3090b8ccd0b07138406fddc34e8123b7feafe74a
werozak <wer.zak12@gmail.com>
2021-06-10 12:52:25 +0200

    Dodanie metod sortowania

--- MyStrategyClear/src/sort/methods/Bubblesort.java
+++ MyStrategyClear/src/sort/methods/Bubblesort.java
@@ -11,4 +11,18 @@ package sort.methods;
  */
 public class Bubblesort {
     /* http://www.algorytm.org/algorytmy-sortowania/sortowanie-babelkowe-bubblesort.html */
+      void sortbab(int[] tab) {
+        int rozmiar = tab.length;
+        int temp = 0;
+        for (int i = 0; i < rozmiar; i++) {
+            for (int j = 1; j < (rozmiar - i); j++) {
+                if (tab[j - 1] > tab[j]) {
+                    temp = tab[j - 1];
+                    tab[j - 1] = tab[j];
+                    tab[j] = temp;
+                }
+            }
+        }
+        // http://www.devprog.pl/sortowanie-babelkowe-tablicy/
+}
 }
--- MyStrategyClear/src/sort/methods/Quicksort.java
+++ MyStrategyClear/src/sort/methods/Quicksort.java
@@ -11,4 +11,34 @@ package sort.methods;
  */
 public class Quicksort {
     /* http://www.algorytm.org/algorytmy-sortowania/sortowanie-szybkie-quicksort/quick-j.html */
+     private int partition(int arr[], int begin, int end) {
+    int pivot = arr[end];
+    int i = (begin-1);
+
+    for (int j = begin; j < end; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+
+            int swapTemp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = swapTemp;
+        }
+    }
+
+    int swapTemp = arr[i+1];
+    arr[i+1] = arr[end];
+    arr[end] = swapTemp;
+
+    return i+1;
+}
+    
+    public void sortqu(int arr[], int begin, int end) {
+    if (begin < end) {
+        int partitionIndex = partition(arr, begin, end);
+
+        sortqu(arr, begin, partitionIndex-1);
+        sortqu(arr, partitionIndex+1, end);
+    }
+}
+    // https://www.baeldung.com/java-quicksort
 }
